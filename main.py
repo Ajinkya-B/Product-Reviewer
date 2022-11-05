0a63c273aeb51b3941495c1d0b2eba5ebbf7b20e
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-11-05 11:59:27 -0400

    added comments on the main functionalities of the backend

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Given bit array data the api should classify the type of pop can
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# the classified can is then sent to the amazon api to get the reviews for it
